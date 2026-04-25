990338edc83ef5670a664b7b15d1ed197fad38b4
Deepak <0eepakverma@gmail.com>
2026-04-26 00:59:04 +0530

    new changes

--- src/config/connectDb.js
+++ src/config/connectDb.js
@@ -1,9 +1,14 @@
-const mongoose = require(mongoose)
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
 
 async function connectDb(){
     try{
-        await mongoose.connect("")
+        await mongoose.connect(process.env.db_url)
+        console.log("conntect to db successfully !!");
+
     }catch(err){
         console.log("error !!\n database connection failed! err: ",err)
     }
 }
+
+module.exports = connectDb;
